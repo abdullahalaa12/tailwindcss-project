5c8322588bd358c94d4eb57875fcb7e1ab9cd7e8
AbdullahHafez <abdullah.alaa12@gmail.com>
2024-09-30 05:37:50 +0300

    Mobile Navebar functionality & animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  content: ["./build/**/*.html", "./build/js/**/*.js"],
   theme: {
     extend: {
+      colors: {
+        papayawhip: {
+          light: "#FEF4E4",
+          DEFAULT: "#FFEFD5",
+          dark: "#FEE5BC",
+        },
+      },
       screens: {
         widescreen: { raw: "(min-aspect-ratio: 3/2)" },
-        tallscreen: { raw: "(min-aspect-ratio: 1/2)" },
+        tallscreen: { raw: "(max-aspect-ratio: 65/100)" },
+      },
+      keyframes: {
+        "open-menu": {
+          "0%": { transform: "scaleY(0)" },
+          "80%": { transform: "scaleY(1.2)" },
+          "100%": { transform: "scaleY(1)" },
+        },
+      },
+      animation: {
+        "open-menu": "open-menu 0.5s ease-in-out forwards",
       },
     },
   },
